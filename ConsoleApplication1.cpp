161ec8b4c39907e4546af5e068aabb60c0ece734
Егор <98447700+pyger72@users.noreply.github.com>
2022-10-16 17:11:12 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, a, b, z, x, sum1, sum2;
+    cin >> n;
+    a = n / 1000;
+    b = n % 1000;
+    sum1 = b % 10 + (b / 10)%10 + b / 100;
+    sum2 = a % 10 + (a / 10) % 10 + a / 100;
+    if (sum1 == sum2) cout << "YES";
+    else cout << "NO";
+}
